7d6d5cf0838afc0c1372c3388bf5eefb4803c8f4
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-01-27 12:50:13 +0200

    Acceptable workout layout.

---
+++ app/build/intermediates/classes/debug/ui/activities/WorkoutActivity$$ViewInjector.java
@@ -0,0 +1,23 @@
+// Generated code from Butter Knife. Do not modify!
+package ui.activities;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+
+public class WorkoutActivity$$ViewInjector {
+  public static void inject(Finder finder, final ui.activities.WorkoutActivity target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131558463, "field 'mStartButton'");
+    target.mStartButton = (android.widget.Button) view;
+    view = finder.findRequiredView(source, 2131558464, "field 'mFinishButton'");
+    target.mFinishButton = (android.widget.Button) view;
+    view = finder.findRequiredView(source, 2131558459, "field 'mPauseButton'");
+    target.mPauseButton = (android.widget.ImageButton) view;
+  }
+
+  public static void reset(ui.activities.WorkoutActivity target) {
+    target.mStartButton = null;
+    target.mFinishButton = null;
+    target.mPauseButton = null;
+  }
+}
--- app/src/main/java/ui/activities/WorkoutActivity.java
+++ app/src/main/java/ui/activities/WorkoutActivity.java
@@ -1,15 +1,69 @@
 package ui.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import bellamica.tech.dreamteenfitness.R;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class WorkoutActivity extends Activity {
+
+    @InjectView(R.id.startButton)
+    Button mStartButton;
+    @InjectView(R.id.finishButton)
+    Button mFinishButton;
+    @InjectView(R.id.pauseButton)
+    ImageButton mPauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        ButterKnife.inject(this);
+
+        mStartButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStartButton.setVisibility(View.GONE);
+                mPauseButton.setVisibility(View.VISIBLE);
+                mFinishButton.setVisibility(View.VISIBLE);
+
+                if (getActionBar() != null) {
+                    getActionBar().hide();
+                }
+                // startUiCountdown();
+            }
+        });
+
+        mPauseButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getActionBar() != null) {
+                    getActionBar().show();
+                }
+                mStartButton.setVisibility(View.VISIBLE);
+                mPauseButton.setVisibility(View.GONE);
+                mFinishButton.setVisibility(View.GONE);
+
+                // stopUiCountdown();
+            }
+        });
+
+        mFinishButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(WorkoutActivity.this, "Workout saved",
+                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(WorkoutActivity.this,
+                        MainActivity.class));
+            }
+        });
     }
 }
