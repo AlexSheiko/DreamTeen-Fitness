ab4f67bd54af7d702f87257f96b8a869ed148c05
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-01-31 21:58:13 +0200

    Calculates distance to store.

--- app/src/main/java/ui/activities/SummaryActivity.java
+++ app/src/main/java/ui/activities/SummaryActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,31 +12,14 @@ import android.widget.EditText;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
-import com.facebook.HttpMethod;
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.sromku.simple.fb.Permission;
-import com.sromku.simple.fb.SimpleFacebook;
-import com.sromku.simple.fb.entities.Score;
-import com.sromku.simple.fb.listeners.OnLoginListener;
-import com.sromku.simple.fb.listeners.OnPublishListener;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import bellamica.tech.dreamteenfitness.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class SummaryActivity extends Activity {
 
-    private static final String TAG = SummaryActivity.class.getSimpleName();
-
     private float mDistance;
     private int mStepCount;
-    private SimpleFacebook mSimpleFacebook;
 
     private SharedPreferences mSharedPrefs;
 
@@ -79,121 +61,6 @@ public class SummaryActivity extends Activity {
             }
         });
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mSimpleFacebook.onActivityResult(this, requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    public void saveRun(View view) {
-        mSharedPrefs.edit().putFloat("Distance", 0).apply();
-
-        if (mStepCount != 0) {
-                mSimpleFacebook = SimpleFacebook.getInstance(this);
-                mSimpleFacebook.login(onLoginListener);
-        } else {
-            startActivity(new Intent(SummaryActivity.this, MainActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-        }
-    }
-
-    OnLoginListener onLoginListener = new OnLoginListener() {
-        @Override
-        public void onLogin() {
-            publishScoreIfBest();
-        }
-
-        @Override
-        public void onNotAcceptingPermissions(Permission.Type type) {
-            // user didn't accept READ or WRITE permission
-            Log.w(TAG, String.format("You didn't accept %s permissions", type.name()));
-        }
-
-        @Override
-        public void onException(Throwable throwable) {
-            Log.e(TAG, "Exception caught: " + throwable.getMessage());
-        }
-
-        @Override
-        public void onFail(String s) {
-            Log.e(TAG, "Login failed. Cause: " + s);
-        }
-
-        @Override
-        public void onThinking() {
-            Log.i(TAG, "Thinking...");
-        }
-    };
-
-    private void publishScoreIfBest() {
-
-        Session session = mSimpleFacebook.getSession();
-            /* make the API call */
-        new Request(
-                session,
-                "/me/scores",
-                null,
-                HttpMethod.GET,
-                new Request.Callback() {
-                    public void onCompleted(Response response) {
-                            /* handle the result */
-                        try {
-                            JSONObject responseBody = new JSONObject(response.getRawResponse());
-                            JSONArray data = responseBody.getJSONArray("data");
-                            JSONObject scoreObj = data.getJSONObject(0);
-                            if (!scoreObj.getString("score").isEmpty()) {
-                                int previousValue = Integer.parseInt(scoreObj.getString("score"));
-
-                                if (mStepCount > previousValue) {
-                                    publishNewScore();
-                                }
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            publishNewScore();
-                        }
-                    }
-                }
-        ).executeAsync();
-    }
-
-    private void publishNewScore() {
-        Score newValue = new Score.Builder()
-                .setScore(mStepCount)
-                .build();
-        mSimpleFacebook.publish(newValue, onPublishListener);
-    }
-
-    OnPublishListener onPublishListener = new OnPublishListener() {
-        @Override
-        public void onComplete(String postId) {
-            Log.i(TAG, "Published successfully");
-
-            startActivity(new Intent(SummaryActivity.this, MainActivity.class)
-                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-        }
-
-        @Override
-        public void onException(Throwable throwable) {
-            super.onException(throwable);
-            Log.e(TAG, throwable.getMessage());
-        }
-
-        @Override
-        public void onFail(String reason) {
-            super.onFail(reason);
-            Log.e(TAG, "Failed to post score: " + reason);
-        }
-
-        @Override
-        public void onThinking() {
-            super.onThinking();
-        }
-    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
