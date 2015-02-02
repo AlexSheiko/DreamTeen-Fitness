caf44a06058c5f1825360cb7feb00d50e7091d94
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-02-02 13:03:18 +0200

    Challenge goal dialog layout.

--- app/src/main/java/ui/activities/ChallengesActivity.java
+++ app/src/main/java/ui/activities/ChallengesActivity.java
@@ -1,9 +1,12 @@
 package ui.activities;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 
 import bellamica.tech.dreamteenfitness.R;
+import ui.fragments.ChallengeGoalDialog;
 
 public class ChallengesActivity extends Activity {
 
@@ -12,5 +15,20 @@ public class ChallengesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenges);
 
+    }
+
+    public void addChallengeGoal(View view) {
+        Bundle bundle = new Bundle();
+        switch (view.getId()) {
+            case R.id.setStepsButton:
+                bundle.putString("key", "steps");
+                break;
+            case R.id.setDurationButton:
+                bundle.putString("key", "duration");
+                break;
+        }
+        DialogFragment newFragment = new ChallengeGoalDialog();
+        newFragment.setArguments(bundle);
+        newFragment.show(getFragmentManager(), "dialog_challenge_goal");
     }
 }
