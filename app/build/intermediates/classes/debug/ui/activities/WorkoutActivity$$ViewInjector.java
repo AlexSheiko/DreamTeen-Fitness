6eed7378841f05a315232ade6e68f1362dfa547c
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-02-02 09:43:40 +0200

    Scroll in description.

--- app/build/intermediates/classes/debug/ui/activities/WorkoutActivity$$ViewInjector.java
+++ app/build/intermediates/classes/debug/ui/activities/WorkoutActivity$$ViewInjector.java
@@ -7,20 +7,20 @@ import butterknife.ButterKnife.Finder;
 public class WorkoutActivity$$ViewInjector {
   public static void inject(Finder finder, final ui.activities.WorkoutActivity target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131558467, "field 'mStartButton'");
+    view = finder.findRequiredView(source, 2131558468, "field 'mStartButton'");
     target.mStartButton = (android.widget.Button) view;
-    view = finder.findRequiredView(source, 2131558468, "field 'mFinishButton'");
+    view = finder.findRequiredView(source, 2131558469, "field 'mFinishButton'");
     target.mFinishButton = (android.widget.Button) view;
-    view = finder.findRequiredView(source, 2131558461, "field 'mPauseButton'");
+    view = finder.findRequiredView(source, 2131558460, "field 'mPauseButton'");
     target.mPauseButton = (android.widget.ImageButton) view;
-    view = finder.findRequiredView(source, 2131558466, "field 'mDurationCounter'");
+    view = finder.findRequiredView(source, 2131558467, "field 'mDurationCounter'");
     target.mDurationCounter = (android.widget.TextView) view;
-    view = finder.findRequiredView(source, 2131558460, "field 'mVideoView'");
-    target.mVideoView = (android.widget.VideoView) view;
-    view = finder.findRequiredView(source, 2131558463, "field 'mTitle'");
-    target.mTitle = (android.widget.TextView) view;
-    view = finder.findRequiredView(source, 2131558465, "field 'mPositionLabel'");
+    view = finder.findRequiredView(source, 2131558464, "field 'mPositionLabel'");
     target.mPositionLabel = (android.widget.TextView) view;
+    view = finder.findRequiredView(source, 2131558462, "field 'mTitleTextView'");
+    target.mTitleTextView = (android.widget.TextView) view;
+    view = finder.findRequiredView(source, 2131558466, "field 'mDescriptionTextView'");
+    target.mDescriptionTextView = (android.widget.TextView) view;
   }
 
   public static void reset(ui.activities.WorkoutActivity target) {
@@ -28,8 +28,8 @@ public class WorkoutActivity$$ViewInjector {
     target.mFinishButton = null;
     target.mPauseButton = null;
     target.mDurationCounter = null;
-    target.mVideoView = null;
-    target.mTitle = null;
     target.mPositionLabel = null;
+    target.mTitleTextView = null;
+    target.mDescriptionTextView = null;
   }
 }
