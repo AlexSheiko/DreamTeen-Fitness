c651ee16065d1935cc9663ef614919f3939a1ca6
AlekseySheyko <aleksey_sheyko@hotmail.com>
2015-02-26 00:38:36 +0200

    Don't crash when first FB login

--- app/src/main/java/ui/activities/FriendsActivity.java
+++ app/src/main/java/ui/activities/FriendsActivity.java
@@ -22,11 +22,12 @@ import android.widget.Toast;
 
 import com.facebook.FacebookException;
 import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.widget.LoginButton;
 import com.facebook.widget.WebDialog;
 import com.google.android.gms.plus.PlusShare;
+import com.sromku.simple.fb.Permission;
+import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.SimpleFacebookConfiguration;
+import com.sromku.simple.fb.listeners.OnLoginListener;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -34,90 +35,95 @@ import java.util.HashSet;
 import bellamica.tech.dreamteenfitness.R;
 
 
-public class FriendsActivity extends Activity
-        implements OnClickListener {
+public class FriendsActivity extends Activity implements OnClickListener {
+
+    protected static final String TAG = FriendsActivity.class.getSimpleName();
 
     private ArrayList<String> mEmails;
     private String[] mRecipients;
 
+    private SimpleFacebook mSimpleFacebook;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        uiHelper = new UiLifecycleHelper(this, callback);
-        uiHelper.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
+                .setAppId(getString(R.string.facebook_app_id))
+                .setNamespace("dreamteen_fitness")
+                .build();
+        SimpleFacebook.setConfiguration(configuration);
 
         Button shareButton = (Button) findViewById(R.id.share_button);
         shareButton.setOnClickListener(this);
 
-        if (Session.getActiveSession() != null && Session.getActiveSession().isOpened()) {
-
-            LoginButton facebookButton = (LoginButton) findViewById(R.id.login_button);
-            facebookButton.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.i("TAG", "Session opened");
-
+        Button facebookButton = (Button) findViewById(R.id.login_button);
+        facebookButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Session.getActiveSession() == null || !Session.getActiveSession().isOpened()) {
+                    mSimpleFacebook.login(onLoginListener);
+                } else {
                     Bundle params = new Bundle();
                     params.putString("message", "DreamTeen Fitness is pretty cool. Check it out on Google Play. See if you can beat my score! http://goo.gl/YdMFVk");
                     showDialogWithoutNotificationBar("apprequests", params);
                 }
-            });
-        }
+            }
+        });
     }
 
-    private UiLifecycleHelper uiHelper;
-    private Session.StatusCallback callback =
-            new Session.StatusCallback() {
-                @Override
-                public void call(Session session,
-                                 SessionState state, Exception exception) {
-                    onSessionStateChange(session);
-                }
-            };
-
-    private void onSessionStateChange(Session session) {
-        if (session.isOpened()) {
-
-            Log.i("TAG", "Session opened");
+    OnLoginListener onLoginListener = new OnLoginListener() {
+        @Override
+        public void onLogin() {
+            // change the state of the button or do whatever you want
+            Log.i(TAG, "Logged in");
 
             Bundle params = new Bundle();
             params.putString("message", "DreamTeen Fitness is pretty cool. Check it out on Google Play. See if you can beat my score! http://goo.gl/YdMFVk");
             showDialogWithoutNotificationBar("apprequests", params);
         }
-    }
+
+        @Override
+        public void onNotAcceptingPermissions(Permission.Type type) {
+            // user didn't accept READ or WRITE permission
+            Log.w(TAG, String.format("You didn't accept %s permissions", type.name()));
+        }
+
+        @Override
+        public void onThinking() {
+        }
+
+        @Override
+        public void onException(Throwable e) {
+            e.printStackTrace();
+        }
+
+        @Override
+        public void onFail(String s) {
+            Log.e(TAG, "Failed to authenticate. Cause: " + s);
+        }
+
+    /*
+     * You can override other methods here:
+     * onThinking(), onFail(String reason), onException(Throwable throwable)
+     */
+    };
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        uiHelper.onActivityResult(requestCode, resultCode, data);
+        try {
+            mSimpleFacebook.onActivityResult(this, requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
+        } catch (Exception e) {
+            Log.w(TAG, e.getMessage());
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        uiHelper.onResume();
-
-        Session.getActiveSession();
-    }
-
-    @Override
-    public void onPause() {
-        uiHelper.onPause();
-        super.onPause();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        uiHelper.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        uiHelper.onDestroy();
+        mSimpleFacebook = SimpleFacebook.getInstance(this);
     }
 
     private WebDialog dialog = null;
@@ -219,7 +225,7 @@ public class FriendsActivity extends Activity
         alertDialog.show();
     }
 
-    public void sendEmail(){
+    public void sendEmail() {
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.setType("message/rfc822");  //set the email recipient
         emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, mRecipients);
@@ -227,7 +233,8 @@ public class FriendsActivity extends Activity
                 "Install DreamTeen Fitness and challenge me! \nhttp://goo.gl/6Kqzvs");
         emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Invitation");
         //let the user choose what email client to use
-        startActivity(Intent.createChooser(emailIntent, "Send mail using...")); }
+        startActivity(Intent.createChooser(emailIntent, "Send mail using..."));
+    }
 
     public ArrayList<String> getNameEmailDetails() {
         ArrayList<String> emlRecs = new ArrayList<String>();
@@ -264,4 +271,3 @@ public class FriendsActivity extends Activity
         return emlRecs;
     }
 }
-
